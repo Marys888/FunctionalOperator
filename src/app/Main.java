b5c557f21d84b3280804bd8fd21582817c47677b
Marys888 <severovamaha1999@gmail.com>
2025-06-15 21:30:12 +0200

    Add sum operation implementation

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,12 @@
 package app;
 
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        MathOperation sum = new MathOperation() {
+            @Override
+            public int operate(int a, int b){
+                return a+b;
+            }
+        };
+    }
 }
--- src/app/MathOperation.java
+++ src/app/MathOperation.java
@@ -1,8 +1,8 @@
 package app;
 
-public interface MathOperation {
+
     @FunctionalInterface
     public interface MathOperation {
         int operate(int a, int b);
-    }
+
 }
